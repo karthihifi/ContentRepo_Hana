ec11b18c15b181ec28336918575d343dab1748e8
karthihifi <karthi.hifi@gmail.com>
2021-11-27 14:33:30 +0000

    cors fixed

--- srv/server.js
+++ srv/server.js
@@ -1,4 +1,13 @@
-const proxy = require('@sap/cds-odata-v2-adapter-proxy')
- const cds = require('@sap/cds')
- cds.on('bootstrap', app => app.use(proxy()))
- module.exports = cds.server
+const proxy = require("@sap/cds-odata-v2-adapter-proxy");
+const cds = require("@sap/cds");
+const cors = require("cors");
+//  cds.on('bootstrap', (app) => {app.use(proxy()
+//     app.use(cors()
+// }))
+
+cds.on("bootstrap", (app) => {
+  console.debug("Use: cors middleware");
+  app.use(proxy());
+  app.use(cors());
+});
+module.exports = cds.server;
